86170e8bac1be71a031d39d5a4e14f45e5c30e3c
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-05-20 16:49:34 +0000

    Added program

--- main.cs
+++ main.cs
@@ -3,12 +3,30 @@
 // Date 2022
 
 using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+        );
+        var json = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        JsonNode jsonData = JsonNode.Parse(response)!;
+        JsonNode temp = jsonData["main"]["temp"];
+        var temperature = (double) temp; 
+        string celsiusTemp = (temperature - 273.15).ToString("0");
+
+        Console.WriteLine("The current weather is " + celsiusTemp + "°C");
+        Console.WriteLine("\nPress any key to quit...");
+        Console.ReadKey();
         Console.WriteLine("\nFinished.");
     }
 }
